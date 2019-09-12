e051834f59030131b5819649937ca0f03257c3b6
Kundan Kumar <kundan.kumar1@in.abb.com>
2019-09-12 14:52:32 +0530

    Add project files.

---
+++ MovieRecomendationApp/Controllers/RecommendationController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using MovieRecomendationApp.Model;
+using MovieRecommender.Models;
+
+namespace MovieRecomendationApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecommendationController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Recommendation engine is up and running");
+        }
+
+        [HttpPost]
+        [Route("Execute")]
+        public ActionResult Execute([FromBody]Input input)
+        {
+            try {
+                string outPut = string.Empty;
+                if (input?.MovieSelection == null || input?.MovieSelection.Split(':').Length != 2)
+                {
+                    return BadRequest("Input data format error");
+                }
+                var movieRatingTestInput = new MovieRating
+                {
+                    userId = Int32.Parse(input.MovieSelection.Split(':')[0]),
+                    movieId = Int32.Parse(input.MovieSelection.Split(':')[1])
+                };
+                MLContext mLContext = new MLContext();
+                DataViewSchema modelSchema;
+                ITransformer trainedModel = mLContext.Model.Load(Path.Combine(Environment.CurrentDirectory, "", "MovieRecommenderModel.zip"), out modelSchema);
+                var predictionFunction = mLContext.Model.CreatePredictionEngine<MovieRating, MovieRatingPrediction>(trainedModel);
+                var movieRatingPrediction = predictionFunction.Predict(movieRatingTestInput);
+
+                if (Math.Round(movieRatingPrediction.Score, 1) > 3.5)
+                {
+                    outPut = String.Concat(movieRatingTestInput.userId, ":", movieRatingTestInput.movieId, ":", true);
+                }
+                else
+                {
+                    outPut = String.Concat(movieRatingTestInput.userId, ":", movieRatingTestInput.movieId, ":", false);
+                }
+
+                return Ok(outPut);
+            } catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ MovieRecomendationApp/Model/Input.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRecomendationApp.Model
+{
+    public class Input
+    {
+        public string MovieSelection { get; set; }
+    }
+}
